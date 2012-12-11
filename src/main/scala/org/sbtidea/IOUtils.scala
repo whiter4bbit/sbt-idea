9743e9b7dcdc5b4bfbf9c4bed28ed4bc8b81db41
Pavel Zalunin <p.zalunin@itransition.com>
2012-12-12 01:47:25 +0300

    careful replacing user.home to environment variable

--- src/main/scala/org/sbtidea/IOUtils.scala
+++ src/main/scala/org/sbtidea/IOUtils.scala
@@ -1,10 +1,19 @@
 package org.sbtidea
 
+import java.io.{File => JFile}
 import sbt.{IO, File}
 
 object IOUtils {
 
-  def replaceUserHome(path: String): String = path.replace(SystemProps.userHome, "$USER_HOME$")
+  def replaceUserHome(path: String): String = { 
+    val userHome = SystemProps.userHome
+    if (path.indexOf(userHome) == 0) {
+      val offset = if (userHome.endsWith(JFile.separator)) 1 else 0
+      "$USER_HOME$" + path.substring(userHome.length - offset)
+    } else {
+      path
+    }
+  }
 
   def relativePath(base: File, file: File, prefix: String) =
     IO.relativize(base, file.getCanonicalFile).map(prefix + _).getOrElse(replaceUserHome(file.getCanonicalPath))
